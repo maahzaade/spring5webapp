dea90400b8f810925a0c7daf6c25c4af04a2ebf7
maahzaade <mahdiye.nikraftar@gmail.com>
2021-05-31 21:09:51 -0400

    35.Dependency Injection using Spring Framework

--- sfg-di/src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -2,6 +2,9 @@ package guru.springframework.sfgdi.services;/*
  * Created by Mahdiye on 2021, May, 28
  */
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
